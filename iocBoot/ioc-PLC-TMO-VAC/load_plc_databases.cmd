68dcccec620f186ffa537881e556aae74ba5fe9c
Allen Pai <adpai@slac.stanford.edu>
2021-02-11 14:12:27 -0800

    DEV: updated ioc to 0.4.1

--- iocBoot/ioc-PLC-TMO-VAC/load_plc_databases.cmd
+++ iocBoot/ioc-PLC-TMO-VAC/load_plc_databases.cmd
@@ -1,1 +1,3 @@
-dbLoadRecords("PLC_TMO_VAC.db", "PORT=ASYN_PLC,PREFIX=PLC_TMO_VAC:,IOCNAME=$(IOCNAME)")
+# Total records: 5954
+callbackSetQueueSize(13908)
+dbLoadRecords("PLC_TMO_VAC.db", "PORT=ASYN_PLC,PREFIX=PLC:TMO:VAC:,IOCNAME=$(IOC),IOC=$(IOC)")
