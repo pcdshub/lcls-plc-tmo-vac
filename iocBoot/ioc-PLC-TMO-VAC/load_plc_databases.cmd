d7f348ee16cbd81efc24c2e266f3fb4a53d692a9
Allen Pai <adpai@slac.stanford.edu>
2021-02-24 19:14:18 -0800

    DEV: updated ioc with the arbiter 2 changes pv prefix=PLC:TMO:VAC:ARB:02

--- iocBoot/ioc-PLC-TMO-VAC/load_plc_databases.cmd
+++ iocBoot/ioc-PLC-TMO-VAC/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 5954
-callbackSetQueueSize(13908)
+# Total records: 6147
+callbackSetQueueSize(14294)
 dbLoadRecords("PLC_TMO_VAC.db", "PORT=ASYN_PLC,PREFIX=PLC:TMO:VAC:,IOCNAME=$(IOC),IOC=$(IOC)")
